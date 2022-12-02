9f88770e36fa5b11069e110a12cf4c405fb3f1cd
Monmoy042 <monmoykms.16@gmail.com>
2022-12-02 15:43:12 +0600

    Counter App in React Native

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Text, StyleSheet, View } from "react-native";
+import CounterApp from "./src/components/CounterApp";
 import Flex from "./src/components/Flex";
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Flex />
+      <CounterApp />
     </View>
   );
 };
---
+++ src/components/CounterApp.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+
+const CounterApp = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <View>
+      <Text style={styles.output}>{count}</Text>
+      <TouchableOpacity
+        style={styles.btnStyle}
+        onPress={() => {
+          setCount(count + 10);
+        }}
+      >
+        <Text style={styles.textStyle}>+10</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.btnStyle}
+        onPress={() => {
+          setCount(count - count);
+        }}
+      >
+        <Text style={styles.textStyle}>Reset</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.btnStyle}
+        onPress={() => {
+          return count > 0 ? setCount(count - 10) : alert("You cannot do this");
+        }}
+      >
+        <Text style={styles.textStyle}>-10</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  output: {
+    backgroundColor: "purple",
+    height: 50,
+    width: 200,
+    display: "block",
+    margin: "auto",
+    textAlign: "center",
+    fontSize: 30,
+    color: "#fff",
+  },
+  btnStyle: {
+    margin: 20,
+    padding: 5,
+    backgroundColor: "#000",
+  },
+  textStyle: {
+    color: "#fff",
+    fontSize: 20,
+    textAlign: "center",
+  },
+});
+
+export default CounterApp;
